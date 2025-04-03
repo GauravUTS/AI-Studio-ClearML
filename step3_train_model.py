1ed4b1bf42e44537b4116f5842ea8d012bd93bbe
GauravUTS <gaurav.chamoli@student.uts.edu.au>
2025-04-03 19:23:50 +1100

    Update step3_train_model.py

--- step3_train_model.py
+++ step3_train_model.py
@@ -14,7 +14,7 @@ logger = Logger.current_logger()
 
 # Arguments
 args = {
-    'dataset_task_id': 'e5d87ceb1090414b889b91d8a7e3ea01',
+    'dataset_task_id': '7652762a42354e44a01b615e73304bba',
 }
 task.connect(args)
 
